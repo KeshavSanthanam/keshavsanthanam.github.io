eb42c8ba69fa53524a918be983c582c7f644bc1d
keshavsanthanam <keshav.r.santhanam@gmail.com>
2026-01-28 01:58:37 -0600

    minimize listings

--- data/projects.js
+++ data/projects.js
@@ -3,11 +3,11 @@ const projectsData = [
     {
         name: 'ETA Reading List',
         description: 'Curated collection of articles, books, and podcasts on search funds and business acquisition',
-        url: '#'
-    },
-    {
-        name: 'Deal Analysis Framework',
-        description: 'Personal framework for evaluating acquisition opportunities and performing due diligence',
-        url: '#'
-    }
+        url: 'https://github.com/KeshavSanthanam/ETA_Resources'
+    }//,
+    // {
+    //     name: 'Deal Analysis Framework',
+    //     description: 'Personal framework for evaluating acquisition opportunities and performing due diligence',
+    //     url: '#'
+    // }
 ];
